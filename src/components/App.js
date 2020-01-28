5a1e2fb18c30e15af22e1e4313f651828f68f347
Minh Vu <minhhv1@uci.edu>
2020-01-27 17:52:12 -0800

    added header

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Cryptocurrency Ticker</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
